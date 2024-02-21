699370c9d02258653b494c9756d2f56da13546d8
Andrey Elmanov <94552760+AndreyElmanov@users.noreply.github.com>
2024-02-21 15:57:46 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={'https://andreyelmanov.github.io/todo-react-ts/'}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
